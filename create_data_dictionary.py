baac71690db0d7873c5e722ef8c75a0328c29681
Vincent Delorme <vincent.delorme@gmail.com>
2017-01-26 13:45:45 +0000

    Change structure of the dictionnary

--- create_data_dictionary.py
+++ create_data_dictionary.py
@@ -10,6 +10,7 @@ emojis_f.close()
 
 # Default meanings and current_meaning to keywords and first keyword 
 for k in emojis_json.keys():
+	emojis_json[k]['id'] = k
 	if 'keywords' in emojis_json[k].keys() and len(emojis_json[k]['keywords']) > 0:
 		# print emojis_json[k]['keywords'][0]
 		emojis_json[k]['meanings'] = emojis_json[k]['keywords']
@@ -23,13 +24,18 @@ def setMeaningsAndDefaultMeaning(id, meanings, default_meaning):
 	emojis_json[id]['meanings'] = meanings
 	emojis_json[id]['currentmeaning'] = default_meaning
 
-
 setMeaningsAndDefaultMeaning('prince', ['the prince', 'the king', 'the knight', 'the boy'], 'the prince')
 setMeaningsAndDefaultMeaning('princess', ['the princess', 'the girl'], 'the princess')
 setMeaningsAndDefaultMeaning('dragon', ['the dragon', 'the monster', 'the enemy'], 'the dragon')
 setMeaningsAndDefaultMeaning('heart', ['love', 'the heart', 'loves'], 'love')
+setMeaningsAndDefaultMeaning('thunder_cloud_rain', ['the storm', 'the rain', 'sad', 'scary'], 'the storm')
+
+output = []
+
+for k in emojis_json.keys():
+	output.append(emojis_json[k])
 
 
 # Write out
 emojis_f = open('./emoji_meaning.json', 'w')
-json.dump(emojis_json, emojis_f)
+json.dump(output, emojis_f)
